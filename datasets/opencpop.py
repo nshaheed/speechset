9443c527924a690cd8acce6fb2d49bfad62654bf
nshaheed <nicholasshaheed@gmail.com>
2023-06-10 16:14:21 -0700

    added opencpop DataReader

---
+++ datasets/opencpop.py
@@ -0,0 +1,54 @@
+import os
+from typing import List, Optional
+
+import numpy as np
+
+from .reader import DataReader
+
+
+class Opencpop(DataReader):
+    """ Opencpop dataset loader. """
+
+    SR = 44100
+
+    def __init__(self, data_dir: str, sr: Optional[int] = None):
+        """Initializer.
+        Args:
+            data_dir: dataset directory.
+            sr: sampling rate.
+        """
+
+        self.sr = sr or VocalSet.SR
+        self.speakers_, self.transcript = self.load_data(data_dir)
+
+    def speakers(self) -> List[str]:
+        return self.speakers_
+
+    def dataset(self) -> List[str]:
+        return self.transcript
+
+    def load_data(self, data_dir: str) -> List[str]:
+        """Load audio. This is sort of backward because atm
+        because VocalSet doesn't have any transcriptions.
+        Args:
+            data_dir: dataset directory.
+        Returns:
+            loaded data, speaker list, transcripts.
+        """
+
+        trans = {}
+        singers = os.listdir(data_dir)
+
+        for root, dirs, files in os.walk(os.path.join(data_dir, singer)):
+            for file in files:
+                if not file.endswith('.wav'):
+                    continue
+
+                fullpath = os.path.join(root, file)
+                # print(f'{fullpath}')
+                # don't need any text where we're going
+                trans[fullpath] = (0, '')
+
+        return singers, trans
+                                       
+            
